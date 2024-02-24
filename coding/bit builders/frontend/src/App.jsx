5304bb34c7fa380cf5831fcdcaae714684022195
Aryan <aryangupta98711@gmail.com>
2024-02-24 11:54:51 +0530

    ad

---
+++ coding/bit builders/frontend/src/App.jsx
@@ -0,0 +1,15 @@
+ 
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <login />
+    
+
+
+    </div>
+  )
+}
+
+export default App
---
+++ coding/bit builders/frontend/src/pages/signup/signup.jsx
@@ -0,0 +1,11 @@
+
+function signup() {
+  return (
+    <div>
+        <h1>sign up</h1>
+      
+    </div>
+  )
+}
+
+export default signup
